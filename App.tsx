53681f12ac6bb3ddc2b9da28473fe8a1162c34c9
Chimie225 <63528890+Chimie225@users.noreply.github.com>
2024-09-30 11:09:30 +0000

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import EquationSolver from './components/EquationSolver';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <EquationSolver />
+    </div>
+  );
+}
+
+export default App;
